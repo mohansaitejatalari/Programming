e34f22963aaaec3ff221a20f882455ba6690fea7
Mohan Sai Teja Talari <mohansaitejatalari02@gmail.com>
2022-10-01 14:38:40 +0530

    armstrong number

---
+++ armstrong.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int power(int x, int y){
+
+    int temp=1;
+    for(int i=1;i<=y;i++) temp *=x;
+    return temp;
+}
+
+int main(){
+
+    int num,len=0;
+    cin>>num;
+    int n = num;
+
+    while(n){
+        len+=1;
+        n/=10;
+    }
+    
+    n = num;
+    int res=0;
+
+    while(n){
+        res = res + power((n%10),len);
+        n/=10;
+    }
+    num==res?cout<<"Armstrong":cout<<"Non Armstrong"<<endl;
+    return 0;
+}
